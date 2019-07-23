9bff0b68a452c16a6cb123b67d5bb8bd938225d7
Saurav Chandra <sauravchandra125@gmail.com>
2019-07-23 22:18:49 +0530

    links added

--- js/style.js
+++ js/style.js
@@ -3,14 +3,8 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 })(jQuery);
-
-$(document).ready(function () {
-    $("nav").find("li").on("click", "a", function () {
-        $('.navbar-collapse.in').collapse('hide');
-    });
-});
